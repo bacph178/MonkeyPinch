f705df9b95a602dc2bdf42e7472c72f7af0062f2
Phùng Hoàng Bắc <phunghoangbac@Phungs-Mac-mini.local>
2015-09-17 14:09:11 +0700

    Pan, Pinch, Rotate

--- MokeyPinch/ViewController.swift
+++ MokeyPinch/ViewController.swift
@@ -19,7 +19,44 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func handlePan(recognizer: UIPanGestureRecognizer) {
+        let translation = recognizer.translationInView(self.view)
+        if let view = recognizer.view {
+            view.center = CGPoint(x: view.center.x + translation.x, y: view.center.y + translation.y)
+        }
+        recognizer.setTranslation(CGPointZero, inView: self.view)
+        if recognizer.state == UIGestureRecognizerState.Ended {
+            let velocity = recognizer.velocityInView(self.view)
+            let magnitude = sqrt((velocity.x * velocity.x) + (velocity.y * velocity.y))
+            let slideMultiplier = magnitude / 200
+            
+            let slideFactor = 0.1 * slideMultiplier
+            
+            var finalPoint = CGPoint(x: recognizer.view!.center.x + (velocity.x * slideFactor),
+                y: recognizer.view!.center.y + (velocity.y * slideFactor))
+            finalPoint.x = min(max(finalPoint.x, 0), self.view.bounds.size.width)
+            finalPoint.y = min(max(finalPoint.y, 0), self.view.bounds.size.height)
+            
+            UIView.animateWithDuration(Double(slideFactor * 2), delay: 0, options: UIViewAnimationOptions.CurveEaseOut, animations: {
+                recognizer.view!.center = finalPoint
+                }, completion: nil)
+        }
+    }
+    
+    @IBAction func handlePinch(recognizer: UIPinchGestureRecognizer) {
+        if let view = recognizer.view {
+            view.transform = CGAffineTransformScale(view.transform, recognizer.scale, recognizer.scale)
+            recognizer.scale = 1
+        }
+    }
+    
+    @IBAction func handleRotate(recognizer: UIRotationGestureRecognizer) {
+        if let view = recognizer.view {
+            view.transform = CGAffineTransformRotate(view.transform, recognizer.rotation)
+            recognizer.rotation = 0
+        }
+    }
 
 }
 
